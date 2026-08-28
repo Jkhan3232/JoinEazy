8849c4de1d5a274a0614cd05d38811987411b551
Jeeshan Khan <jeeshankhan3232@gmail.com>
2026-08-28 16:20:06 +0530

    fix cors problem

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1";
+const API_URL =
+  import.meta.env.VITE_API_URL || "https://joineazy.pages.dev/api/v1";
 const TOKEN_KEY = "12wedfgzxdrftgyhujctvygbhujtvybunij";
 
 let unauthorizedHandler = null;
