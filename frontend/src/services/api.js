52763a3f093de3f268f4582c2cf04477f1e4ca4a
Jeeshan Khan <jeeshankhan3232@gmail.com>
2026-08-28 06:40:05 +0530

    fix logger problem

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1/api/v1";
+  import.meta.env.VITE_API_URL ||
+  "https://backend-delta-dun-31.vercel.app/api/v1";
 const TOKEN_KEY = "12wedfgzxdrftgyhujctvygbhujtvybunij";
 
 let unauthorizedHandler = null;
